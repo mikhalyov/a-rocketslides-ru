08a0742cc75b2dcf9885d1265f5461b4a5294d6e
Victor Kotseruba <barbuzaster@gmail.com>
2014-02-28 03:16:55 +0400

    навигация, отслеживание нажатой кнопки, шрифты на не-retina экранах

--- main.js
+++ main.js
@@ -18,22 +18,11 @@
     return li.find(".person").css("background-image", "url(" + logoUrl + ")");
   });
 
-  $("#reviews ol li").each(function() {
-    var idx;
-    idx = $(this).index();
-    return $(this).click(function() {
-      var slide;
-      $(this).siblings().removeClass("active");
-      $(this).addClass("active");
-      slide = $("#reviews ul li").get(idx);
-      $(slide).prevAll().removeClass("right").addClass("left");
-      $(slide).nextAll().removeClass("left").addClass("right");
-      return $(slide).removeClass("left").removeClass("right");
-    });
-  });
-
   $("a[href='#order-presentation']").click(function() {
+    var button;
     $("#order").addClass("visible");
+    button = $(this).data("name");
+    $("#order [type=submit]").attr("data-name", button).data("name", button);
     return false;
   });
 
@@ -42,11 +31,29 @@
     return false;
   });
 
+  $("#header nav a").click(function() {
+    var duration, scrollTop;
+    scrollTop = $($(this).attr("href")).offset().top;
+    duration = Math.abs(window.scrollY - scrollTop);
+    $("html, body").bind("mousewheel DOMMouseScroll", function() {
+      return false;
+    });
+    $("html, body").animate({
+      scrollTop: scrollTop
+    }, {
+      duration: duration,
+      complete: function() {
+        return $("html, body").unbind("mousewheel DOMMouseScroll");
+      }
+    });
+    return false;
+  });
+
   Mandrill = new mandrill.Mandrill("eryR6LS4JOYJiGZpkOjECw");
 
   sendMail = function(_arg, complete) {
-    var email, message, name, params, phone;
-    name = _arg.name, phone = _arg.phone, email = _arg.email, message = _arg.message;
+    var button, email, message, name, params, phone;
+    name = _arg.name, phone = _arg.phone, email = _arg.email, message = _arg.message, button = _arg.button;
     params = {
       template_name: "request_presentation",
       template_content: [
@@ -62,6 +69,9 @@
         }, {
           name: "message",
           content: message
+        }, {
+          name: "button",
+          content: button
         }
       ],
       message: {
@@ -84,21 +94,35 @@
   };
 
   $("form").submit(function() {
-    var email, message, name, phone;
-    name = $(this).find("[name='name']").val();
-    phone = $(this).find("[name='phone']").val();
-    email = $(this).find("[name='email']").val();
-    message = $(this).find("[name='message']").val();
+    var button, email, message, name, phone;
+    name = $(this).find("[name='name']").val() || "";
+    phone = $(this).find("[name='phone']").val() || "";
+    email = $(this).find("[name='email']").val() || "";
+    message = $(this).find("[name='message']").val() || "";
+    button = $(this).find("[data-name]").data("name") || "";
     sendMail({
       name: name,
       phone: phone,
       email: email,
-      message: message
+      message: message,
+      button: button
     });
     return false;
   });
 
   $(window).scroll($.throttle(100, function() {
+    var found;
+    found = null;
+    $("#header nav a").each(function() {
+      if (window.scrollY > $($(this).attr("href")).offset().top - 100) {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        return found = true;
+      }
+    });
+    if (!found) {
+      $("#header nav a").removeClass("active");
+    }
     if (window.scrollY > 100) {
       return $("#header").addClass("small");
     } else {
